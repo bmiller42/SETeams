1dc45ecd68f8779ada2e5a1233c835235d2496f4
learneman <nuelapia@gmail.com>
2017-02-15 22:24:03 -0800

    Adding dummy BallzyApp that returns empty text

---
+++ BallzyApp/index.ios.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class BallzyApp extends Component {
+  render() {
+    return (
+      <Text></Text>
+    );
+  }
+}
+
+AppRegistry.registerComponent('BallzyApp', () => BallzyApp);
