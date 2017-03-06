8ca576508bf380e837a56d8e17ff3e253bd22f2e
Inderdeep Sandhar <isandhar@mail.csuchico.edu>
2017-03-05 19:51:06 -0800

    adding the videos page

--- BallzyApp/src/components/videos/Videos.js
+++ BallzyApp/src/components/videos/Videos.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import {
-  Text,
+  StyleSheet,
+  Button,
   View,
+  Text,
+  Alert,
+  Navigator
 } from 'react-native';
 
 class Videos extends React.Component {
   render() {
     return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'flex-start', backgroundColor: 'white',}}>
+      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-between'}}>
       <Text>Hello from videos</Text>
+      <View style={styles.button}>
+       <Button onPress={onPress} title="Camera"/>
+     </View>
+     </View>
+   </View>
     );
   }
 }
 
+const onPress = () => {
+  Alert.alert('We are working on the upload of videos');
+};
+
+const styles = StyleSheet.create({
+button: {
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  backgroundColor: 'white',
+  borderRadius: 10,
+  padding: 10,
+  height: 50,
+}
+});
 export default Videos;
