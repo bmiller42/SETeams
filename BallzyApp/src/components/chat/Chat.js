f6dc1e8615a801730d44a3d0a5a96cd27f95a8df
Carlos Perez <cperez44@mail.csuchico.edu>
2017-03-09 13:04:26 -0800

    cperez44 made changes to Chat.js

--- BallzyApp/src/components/chat/Chat.js
+++ BallzyApp/src/components/chat/Chat.js
@@ -1,21 +1,69 @@
-import React, {Component} from 'react';
+import React from 'react';
 import NavigationBar from '../navigationbar/NavigationBar';
 import {
   Text,
   View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
-class Chat extends Component {
+import{
+  Actions,
+  } from 'react-native-router-flux';
+
+class Chat extends React.Component {
+  state = {
+    name: '',
+  };
   render() {
     return(
-      <View style={{flex:1}}>
-        <View style={{flex: 10}}>
-          <Text>Hello from Chat</Text>
-        </View>
+      <View>
+          <Text style={styles.title}> 
+            Enter your name: 
+          </Text>
+          <TextInput
+            style = {styles.nameInput}
+            placeholder='ex: John Snow'
+            onChangeText={(text) => {
+              this.setState({
+                name: text,
+              });
+              }}
+              value={this.state.name}
+           />
+           <TouchableOpacity
+            onPress={() => {
+              console.log(this.state.name);
+            }}
+           >
+            <Text style={styles.buttonText}>
+              NExt
+            </Text>
+           </TouchableOpacity>
         <NavigationBar/>
       </View>
-    )
+    );
   }
 }
+
+var styles = StyleSheet.create({
+  title: {
+    marginTop:20,
+    marginLeft: 20,
+    fontSize: 20,
+  },
+  nameInput: {
+  padding: 5,
+  height: 40,
+  borderWidth: 2,
+  borderColor: 'black',
+  margin: 20,
+  },
+  buttonText: {
+    marginLeft: 20,
+    fontSize: 20,
+  }
+});
 
 export default Chat;
