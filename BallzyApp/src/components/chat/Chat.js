62fff06345aab0e93c6d2b8050edb82942503cd9
Carlos Perez <cperez44@mail.csuchico.edu>
2017-04-12 17:00:05 -0700

    adding design to chat interface

--- BallzyApp/src/components/chat/Chat.js
+++ BallzyApp/src/components/chat/Chat.js
@@ -18,54 +18,89 @@ class Chat extends React.Component {
   };
   render() {
     return(
-      <View style={{flex: 1}}>
-        <View style={{flex:10}}>
-          <Text style={styles.title}> 
-            Enter your name: 
+      <View style={styles.container}>
+        <View style={styles.loginContainer}>
+          <Text style={styles.title}>
+            Enter user nickname!
           </Text>
           <TextInput
             style = {styles.nameInput}
-            placeholder='ex: John Snow'
-            onChangeText={(text) => {
-              this.setState({
-                name: text,
-              });
-              }}
-              value={this.state.name}
-           />
-           <TouchableOpacity
-            onPress={() => {
-              console.log(this.state.name);
+            placeholder='EX: Skankhunt42'
+            onChangeText={ (text) => { this.setState({name: text}); } }
+            value = {this.state.name}
+          />
+          <TouchableOpacity
+            style = {styles.button}
+            onPress = {() => {
+            console.log(this.state.name);
             }}
-           >
-            <Text style={styles.buttonText}>
+          >
+            <Text style = {styles.buttonText}>
               Next
             </Text>
-           </TouchableOpacity>
-          </View>
+          </TouchableOpacity>
+        </View>
         <NavigationBar/>
       </View>
     );
   }
 }
-
-var styles = StyleSheet.create({
+//this a custom prop
+var styles = StyleSheet.create( {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#00BFFF'
+  },
+  loginContainer: {
+    flex: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   title: {
-    marginTop:20,
-    marginLeft: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    //marginTop:20,
+    //marginLeft: 20,
     fontSize: 20,
+    width: 250,
+    backgroundColor: '#FFFF31',
+    borderWidth: 1,
+    borderRadius: 5,
   },
   nameInput: {
-  padding: 5,
-  height: 40,
-  borderWidth: 2,
-  borderColor: 'black',
-  margin: 20,
+    padding: 5,
+    width: 250,
+    height: 40,
+    borderWidth: 2,
+    borderColor: 'black',
+    margin: 20,
+    backgroundColor: 'white'
+  },
+  button: {
+    //marginLeft
+    //justifyContent: 'center',
+    //alignItems: 'center',
+    height: 50,
+    width: 250,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#7F8284',
+    padding: 10,
+    marginTop: 5,
+    backgroundColor: '#AEB3B7'
   },
   buttonText: {
-    marginLeft: 20,
+    //marginLeft: 20,
     fontSize: 20,
+    fontWeight: '600',
+    //height: 50,
+    width: 50,
+    alignSelf: 'center',
+    textAlign: 'center',
+    color: '#FFFFFF'
   }
-});
+} );
 
 export default Chat;
